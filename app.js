3a3e9d2f08789a38b355f859da5e8e4fbb06be39
Dan Lin <dlin2102@gmail.com>
2017-04-13 00:54:46 -0400

    added contact info

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-$(document).ready(function () {
-  $('#myPortfolio').addClass('hidden')
-  $('#aboutMe').addClass('hidden')
-
-  $('#portfolio').on('click', function(){
-    $('#myPortfolio').removeClass('hidden')
-    $('#aboutMe').addClass('hidden')
-    $('#homepage').addClass('hidden')
-  })
-
-  $('#me').on('click', function(){
-    $('#aboutMe').removeClass('hidden')
-    $('#myPortfolio').addClass('hidden')
-    $('#homepage').addClass('hidden')
-  })
-
-  $('#home').on('click', function(){
-    $('#homepage').removeClass('hidden')
+$(document).ready(function() {
     $('#myPortfolio').addClass('hidden')
     $('#aboutMe').addClass('hidden')
-  })
+    $('#contactInfo').addClass('hidden')
+
+    $('#portfolio').on('click', function() {
+        $('#myPortfolio').removeClass('hidden')
+        $('#aboutMe').addClass('hidden')
+        $('#homepage').addClass('hidden')
+        $('#contactInfo').addClass('hidden')
+    })
+
+    $('#me').on('click', function() {
+        $('#aboutMe').removeClass('hidden')
+        $('#myPortfolio').addClass('hidden')
+        $('#homepage').addClass('hidden')
+        $('#contactInfo').addClass('hidden')
+    })
+
+    $('#home').on('click', function() {
+        $('#homepage').removeClass('hidden')
+        $('#myPortfolio').addClass('hidden')
+        $('#aboutMe').addClass('hidden')
+        $('#contactInfo').addClass('hidden')
+    })
+
+    $('#contact').on('click', function() {
+      $('#contactInfo').removeClass('hidden')
+      $('#myPortfolio').addClass('hidden')
+      $('#aboutMe').addClass('hidden')
+      $('#homepage').addClass('hidden')
+    })
 })
