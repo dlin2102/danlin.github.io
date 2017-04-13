945992fb7a6ba695616dfb79d59a8c492cb76df7
Dan Lin <dlin2102@gmail.com>
2017-04-12 20:03:10 -0400

    fixed display class

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-$('#myPortfolio').addClass('hidden')
-$('#aboutMe').addClass('hidden')
-
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 282) {
-    $('.nav').addClass('stuck');
-    $('.subpage').addClass('scroll')
-  } else {
-    $('.nav').removeClass('stuck');
-    $('.subpage').removeClass('scroll')
-  }
-});
-
-$('#portfolio').on('click', function(){
-  $('#myPortfolio').removeClass('hidden')
-  $('#aboutMe').addClass('hidden')
-  $('#homepage').addClass('hidden')
-})
-
-$('#me').on('click', function(){
-  $('#aboutMe').removeClass('hidden')
-  $('#myPortfolio').addClass('hidden')
-  $('#homepage').addClass('hidden')
-})
-
-
-$('#home').on('click', function(){
-  $('#homepage').removeClass('hidden')
+$(document).ready(function () {
   $('#myPortfolio').addClass('hidden')
   $('#aboutMe').addClass('hidden')
+
+  $('#portfolio').on('click', function(){
+    $('#myPortfolio').removeClass('hidden')
+    $('#aboutMe').addClass('hidden')
+    $('#homepage').addClass('hidden')
+  })
+
+  $('#me').on('click', function(){
+    $('#aboutMe').removeClass('hidden')
+    $('#myPortfolio').addClass('hidden')
+    $('#homepage').addClass('hidden')
+  })
+
+  $('#home').on('click', function(){
+    $('#homepage').removeClass('hidden')
+    $('#myPortfolio').addClass('hidden')
+    $('#aboutMe').addClass('hidden')
+  })
 })
