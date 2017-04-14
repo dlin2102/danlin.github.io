378dc282647ad44f0041d747c9e2decbcc784ad3
Dan Lin <dlin2102@gmail.com>
2017-04-13 22:54:57 -0400

    add icons and nav bar

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ $(document).ready(function() {
         $('#homepage').removeClass('hidden')
         $('#myPortfolio').addClass('hidden')
         $('#aboutMe').addClass('hidden')
-        $('#contactInfo').addClass('hidden')
     })
-
+    var scroll_start = 0;
+       var startchange = $('.nav');
+       var offset = startchange.offset();
+       $(document).scroll(function() {
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+              $('.nav').css('background-color', 'rgba(34,34,34,0.9)');
+           } else {
+              $('.nav').css('background-color', 'transparent');
+           }
+       });
 })
